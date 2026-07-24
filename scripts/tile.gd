4d726b6a6cae24ce9bcaf269e6619c0ce01fa1d9
Zachary Ellison <z@zellis.app>
2026-07-24 14:56:09 -0400

    sparkles updates, tile juice

--- scripts/tile.gd
+++ scripts/tile.gd
@@ -8,33 +8,30 @@ var grid_position:Vector2i
 # Highlight tile when hovering mouse
 
 func _on_mouse_entered():
-	
 	var tween = create_tween().set_parallel(true)
-	tween.tween_property($Sprite2D, "scale", Vector2(1.1, 1.1), 0.1)
-	tween.tween_property($Sprite2D, "modulate", Color(1.2, 1.2, 1.2), 0.1) # Brighten
+	tween.tween_property($Sprite2D, "scale", Vector2(1.1, 1.1), 0.05)
+	tween.tween_property($Sprite2D, "modulate", Color(1.2, 1.2, 1.2), 0.50) # Brighten
 
 # Return to default state when mouse exits
-
 func _on_mouse_exited():
-	
-	var tween = create_tween().set_parallel(true)
-	tween.tween_property($Sprite2D, "scale", Vector2(1.0, 1.0), 0.1)
-	tween.tween_property($Sprite2D, "modulate", Color(1, 1, 1), 0.1)
+	reset_tween()
 
-# Set piece them when initializing
-
+# Set piece text when initializing
 func set_tile_type(id: String, texture: Texture2D):
-	
 	type = id
 	$Sprite2D.texture = texture
 
 # Letting the main code know when a tile has been pressed
-
 func _input_event(_viewport, event, _shape_idx):
-	
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			tile_pressed.emit(grid_position)
+			var tween = create_tween().set_parallel(true)
+			tween.tween_property($Sprite2D, "scale", Vector2(0.8, 0.8), 0.05)
+			tween.tween_property($Sprite2D, "modulate", Color(0.75, 0.75, 0.75), 0.1)
+		else:
+			reset_tween()
+				
 
 # Animations when tile is moving
 
@@ -53,5 +50,11 @@ func move_to(target_position: Vector2, play_sound: bool = true):
 # Audio that plays after the tile lands on the board
 
 func _on_move_finished():
-	
 	Audio.play("res://sounds/tile-land.ogg", false, 1.2 - (grid_position.y * 0.05), 0.2)
+	
+func reset_tween():
+	var tween = create_tween().set_parallel(true)
+	tween.tween_property($Sprite2D, "scale", Vector2(1.0, 1.0), 0.1)
+	tween.tween_property($Sprite2D, "modulate", Color(1, 1, 1), 0.1)
+
+	
